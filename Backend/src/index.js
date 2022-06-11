f87da21052a701ed1252faa87947affc05d1b0bf
sivaji <pandasivaji@gmail.com>
2022-06-11 17:37:29 +0530

    update
    updates

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -11,16 +11,15 @@ app.use(express.static("public"))
 app.use(cors());
 
 const UserController = require('./controllers/user.controller')
-const MobileController = require('./controllers/mobile.controller')
-const IpadController = require('./controllers/Ipad.controller')
-const LaptopController = require('./controllers/laptop.controller')
-const CartMobileController = require("./controllers/cart.controller")
+const ProductController = require('./controllers/product.controller')
+
+
 
 app.use("/auth",UserController);
-app.use("/mobiles",MobileController);
-app.use("/Ipads",IpadController);
-app.use("/laptops",LaptopController);
-app.use("/mobilecart",CartMobileController)
+app.use("/products", ProductController);
+
+
+
 
 
 
